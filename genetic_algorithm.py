5ec39a25195cc222f555722f87a9f4e01785ff3d
Yaak3 <brunodesousa656@gmail.com>
2023-11-06 22:55:23 -0300

    testes com a matriz de distâncias

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -2,8 +2,10 @@ import numpy as np
 import random
 import math
 
-distances_matrix = np.transpose(np.loadtxt('distances_between_cells.mat'))
-chromosomes_matrix = np.zeros((20, 20), dtype=float)
+distances_matrix = np.loadtxt('distances_between_cells.mat')
+chromosomes_matrix = np.zeros((20, 20), dtype=int)
+number_of_repetitions = 1
+count = 0
 
 def initialize_genetic_algorithm(chromosomes_matrix, distances_matrix) -> None:
     initialize_chromosomes(chromosomes_matrix)
@@ -12,9 +14,34 @@ def initialize_genetic_algorithm(chromosomes_matrix, distances_matrix) -> None:
 
 def initialize_chromosomes(chromosomes_matrix) -> None:
     for row in range(len(chromosomes_matrix)):
-        chromosomes_matrix[row] = np.random.choice(np.arange(21), size=len(chromosomes_matrix), replace=False)
+        chromosomes_matrix[row] = np.random.choice(np.arange(20), size=len(chromosomes_matrix), replace=False)
+
+    return chromosomes_matrix
 
 def calculate_fitness(chromosomes_matrix, distances_matrix) -> None:
+    distances  = []
+    total_distance = 0
+
+    for chromosome in chromosomes_matrix:
+        
+        for index in range(len(chromosome)):
+            if(index == len(chromosome) - 1):
+                total_distance += execute_fitness_calculation(distances_matrix[0][chromosome[-1]], 
+                                                              distances_matrix[0][chromosome[-1]],
+                                                              distances_matrix[1][chromosome[0]],
+                                                              distances_matrix[1][chromosome[0]])
+            else:
+                total_distance += execute_fitness_calculation(distances_matrix[0][chromosome[index]], 
+                                                              distances_matrix[0][chromosome[index]],
+                                                              distances_matrix[1][chromosome[index + 1]],
+                                                              distances_matrix[1][chromosome[index + 1]])
+                
+        
+        distances.append(total_distance)
+        total_distance = 0
+    
+    return [np.transpose(distances)]
+    '''
     fitness_utility_matrix = np.zeros((20, 1), dtype=float)
     fitness_unitary_results = []
 
@@ -30,6 +57,7 @@ def calculate_fitness(chromosomes_matrix, distances_matrix) -> None:
         fitness_unitary_results.clear()
 
     chromosomes_matrix = np.hstack((chromosomes_matrix, fitness_utility_matrix))
+    '''
 
 def randomize_distances(distances_matrix):
     for distance in distances_matrix:
@@ -43,4 +71,12 @@ def ordering_by_fitness_desc(chromosomes_matrix) -> None:
     ordering_indexes = np.argsort(chromosomes_matrix[:, -1])[::-1]
     chromosomes_matrix = chromosomes_matrix[ordering_indexes]
 
-initialize_genetic_algorithm(chromosomes_matrix, distances_matrix)
+
+while count < number_of_repetitions:
+    chromosomes_matrix = initialize_chromosomes(chromosomes_matrix)
+    distances = calculate_fitness(chromosomes_matrix, distances_matrix)
+    print(distances)
+    chromosomes_matrix = np.hstack((chromosomes_matrix, distances))
+
+    print(chromosomes_matrix)
+    count += 1
